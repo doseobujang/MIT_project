f8500e5d650a241c74060ec272b5894f3174ae25
doseobujang <157891327+doseobujang@users.noreply.github.com>
2024-02-03 10:26:16 +0900

    Add files via upload

---
+++ Introduction to Computer Science and Programming in Python/lec1/ps0/ps0.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+x = int(input())
+y = int(input())
+print(x**y)
+print(int(np.log2(x)))
---
+++ Introduction to Computer Science and Programming in Python/lec1/script/1.py
@@ -0,0 +1,15 @@
+import math
+
+print(6+4*10)
+print((6+4)*10)
+
+print(23.0**5)
+
+# 34*x^2 + 68*x - 510
+a = 34
+b = 68
+c = -510
+x1 = (-b + math.sqrt(b*b - 4*a*c))/(2*a)
+print(x1)
+
+print(math.cos(3.4)**2 + math.sin(3.4)**2)
